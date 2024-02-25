f60c65c138d9eaa346e1d581d5830bbdfaa99a74
minji0626 <jo010163@naver.com>
2024-02-25 20:36:15 +0900

    2

---
+++ SSIT_Java/src/s2/variable/IntegerLiteralExample.java
@@ -0,0 +1,10 @@
+package s2.variable;
+
+public class IntegerLiteralExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- SSIT_Java/src/s2/variable/VariableExchangeExample.java
+++ SSIT_Java/src/s2/variable/VariableExchangeExample.java
@@ -3,8 +3,14 @@ package s2.variable;
 public class VariableExchangeExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int x = 3;
+		int y = 5;
+		System.out.println("x: " + x + " y: " + y );
+		
+		int temp = x;
+		x = y ;
+		y = temp;
+		System.out.println("x: " + x + " y: " + y);
 	}
 
 }
