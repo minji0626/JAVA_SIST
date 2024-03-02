ea5fac2150affbda97971ef1bac293971057cb3d
minji0626 <jo010163@naver.com>
2024-03-02 23:03:11 +0900

    .

---
+++ SSIT_Java/src/s2/variable/CharExample.java
@@ -0,0 +1,17 @@
+package s2.variable;
+
+public class CharExample {
+	public static void main(String[] args) {
+			char c1 = 'A'; //문자 저장
+			char c2 = 65; //해당 문자의 유니코드 저장
+			
+			char c3 = '가'; // 문자 저장
+			char c4 = 44032; // 해당 문자의 유니코드 저장
+			
+			System.out.println(c1);
+			System.out.println(c2);
+			System.out.println(c3);
+			System.out.println(c4);
+	}
+
+}
