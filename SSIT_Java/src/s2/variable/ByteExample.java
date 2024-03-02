6c4b4db8ffbb56739b8363e13f7bedb7bf82a32f
minji0626 <jo010163@naver.com>
2024-03-02 23:03:16 +0900

    .

---
+++ SSIT_Java/src/s2/variable/ByteExample.java
@@ -0,0 +1,19 @@
+package s2.variable;
+
+public class ByteExample {
+	public static void main(String[] args) {
+		byte var1 = -128;
+		byte var2 = -30;
+		byte var3 = 0;
+		byte var4 = 30;
+		byte var5 =127;
+		//byte var6 = 128; byte의 크기보다 크기 때문에 컴파일 오류가 생긴다.
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+	}
+
+}
