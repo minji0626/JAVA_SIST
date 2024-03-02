5e6336b543e563dc0d76286a31ecd22f97e4f25d
minji0626 <jo010163@naver.com>
2024-03-02 23:02:54 +0900

    .

--- SSIT_Java/src/s2/variable/IntegerLiteralExample.java
+++ SSIT_Java/src/s2/variable/IntegerLiteralExample.java
@@ -3,8 +3,17 @@ package s2.variable;
 public class IntegerLiteralExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int var1 = 0b1011;  	// 2진수
+		int var2 = 0206 ;  	// 8진수
+		int var3 = 365;		// 10진수
+		int var4 = 0xB3;		// 16진수
 
+		System.out.println("var1 : " + var1);
+		System.out.println("var2 : " + var2 );
+		System.out.println("var3 : " + var3);
+		System.out.println("var4 : " + var4);
+		
 	}
 
 }
---
+++ SSIT_Java/src/s2/variable/LongExample.java
@@ -0,0 +1,15 @@
+package s2.variable;
+
+public class LongExample {
+	public static void main(String[] args) {
+		long var1 = 10;
+		long var2 = 20L;
+		//long var3 = 1000000000000;	// int로 간주되기 때문에 에러 발생
+		long var4 = 1000000000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var4);
+	}
+
+}
